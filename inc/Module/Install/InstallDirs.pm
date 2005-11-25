580ae1aafc2fc98a0fdd05ecd16a68d5c7212648
Graham Barr <gbarr@pobox.com>
2005-11-25 16:21:18 +0000

    Release 1.18

---
+++ inc/Module/Install/InstallDirs.pm
@@ -0,0 +1,31 @@
+#line 1 "inc/Module/Install/InstallDirs.pm - /home/gbarr/perl/Module/Install/InstallDirs.pm"
+# Copyright (c) 2003 Graham Barr <gbarr@pobox.com>. All rights reserved.
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+
+package Module::Install::InstallDirs;
+use Module::Install::Base; @ISA = qw(Module::Install::Base);
+use Config;
+
+$VERSION = '0.01';
+use strict;
+
+sub installdirs {
+  my $self = shift;
+  my @dirs = ($Config{privlib},$Config{archlib});
+  foreach my $module (@_) {
+    (my $path = "$module.pm") =~ s,::,/,g;
+    foreach my $dir (@dirs) {
+      if (-f "$dir/$path") {
+	$self->makemaker_args(INSTALLDIRS => 'perl');
+	return;
+      }
+    }
+  }
+}
+
+1;
+
+__END__
+
+#line 58
