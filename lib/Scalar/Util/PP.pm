69ab4eac545f16a0fa05d58d663083819a7618b7
Graham Barr <gbarr@pobox.com>
2009-11-14 08:38:10 -0600

    RT#48550 fix pure perl looks_like_number not to match non-ascii digits
    Pure Perl version of looks_like_number thinks Unicode digits are numbers
    
    The pure Perl version uses \d in looks_like_number. \d matches more
    than [0-9] in 5.8 and later, so the pure Perl version incorrectly
    reports "\x{1815}" (MONGOLIAN DIGIT FIVE) as a number. I have attached
    a patch that changes the pure Perl version to use [0-9] instead of \d in
    looks_like_number and added a test to t/lln.t.

--- lib/Scalar/Util/PP.pm
+++ lib/Scalar/Util/PP.pm
@@ -98,8 +98,8 @@ sub looks_like_number {
     require overload;
     return overload::Overloaded($_) ? defined(0 + $_) : 0;
   }
-  return 1 if (/^[+-]?\d+$/); # is a +/- integer
-  return 1 if (/^([+-]?)(?=\d|\.\d)\d*(\.\d*)?([Ee]([+-]?\d+))?$/); # a C float
+  return 1 if (/^[+-]?[0-9]+$/); # is a +/- integer
+  return 1 if (/^([+-]?)(?=[0-9]|\.[0-9])[0-9]*(\.[0-9]*)?([Ee]([+-]?[0-9]+))?$/); # a C float
   return 1 if ($] >= 5.008 and /^(Inf(inity)?|NaN)$/i) or ($] >= 5.006001 and /^Inf$/i);
 
   0;
