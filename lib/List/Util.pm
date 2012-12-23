da485bbca02f861fad44541b457cd460ec948d19
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2012-12-23 16:00:19 +0000

    Bump version to 1.26_001; updated Changes notes

--- lib/List/Util.pm
+++ lib/List/Util.pm
@@ -13,7 +13,7 @@ require Exporter;
 
 our @ISA        = qw(Exporter);
 our @EXPORT_OK  = qw(first min max minstr maxstr reduce sum sum0 shuffle);
-our $VERSION    = "1.26";
+our $VERSION    = "1.26_001";
 our $XS_VERSION = $VERSION;
 $VERSION    = eval $VERSION;
 
