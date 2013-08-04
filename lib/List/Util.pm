ac86b5be39fabf4a890fe148a760b0ef20a6edc6
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2013-08-04 17:05:01 +0100

    Bump VERSION, update Changes notes

--- lib/List/Util.pm
+++ lib/List/Util.pm
@@ -13,7 +13,7 @@ require Exporter;
 
 our @ISA        = qw(Exporter);
 our @EXPORT_OK  = qw(first min max minstr maxstr reduce sum sum0 shuffle pairmap pairgrep pairfirst pairs pairkeys pairvalues);
-our $VERSION    = "1.29";
+our $VERSION    = "1.29_001";
 our $XS_VERSION = $VERSION;
 $VERSION    = eval $VERSION;
 
