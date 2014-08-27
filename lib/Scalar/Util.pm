0657eec15f4a61e544605c67045dbddc191d090d
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-08-27 22:00:22 +0100

    Bumped version to 1.39_004; updated Changes notes

--- lib/Scalar/Util.pm
+++ lib/Scalar/Util.pm
@@ -17,7 +17,7 @@ our @EXPORT_OK = qw(
   dualvar isdual isvstring looks_like_number openhandle readonly set_prototype
   tainted
 );
-our $VERSION    = "1.39_003";
+our $VERSION    = "1.39_004";
 $VERSION   = eval $VERSION;
 
 our @EXPORT_FAIL;
--- lib/Sub/Util.pm
+++ lib/Sub/Util.pm
@@ -16,7 +16,7 @@ our @EXPORT_OK = qw(
   subname set_subname
 );
 
-our $VERSION    = "1.39_003";
+our $VERSION    = "1.39_004";
 $VERSION   = eval $VERSION;
 
 =head1 NAME
