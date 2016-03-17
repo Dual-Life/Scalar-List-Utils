92ce58bebf6cd3ccbdb249c16b48741bd6005061
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2016-03-17 17:28:25 +0000

    Bumped version to 1.43_005; updated Changes notes

--- lib/List/Util.pm
+++ lib/List/Util.pm
@@ -15,7 +15,7 @@ our @EXPORT_OK  = qw(
   all any first min max minstr maxstr none notall product reduce sum sum0 shuffle uniq uniqnum
   pairs unpairs pairkeys pairvalues pairmap pairgrep pairfirst
 );
-our $VERSION    = "1.43_004";
+our $VERSION    = "1.43_005";
 our $XS_VERSION = $VERSION;
 $VERSION    = eval $VERSION;
 
