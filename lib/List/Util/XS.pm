7f54b6d56c157d53cde0cfe4f2ef84b048d66be9
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2024-10-18 12:16:47 +0100

    Bumped version to 1.67; updated Changes notes

--- lib/List/Util/XS.pm
+++ lib/List/Util/XS.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use List::Util;
 
-our $VERSION = "1.66";       # FIXUP
+our $VERSION = "1.67";       # FIXUP
 $VERSION =~ tr/_//d;         # FIXUP
 
 1;
--- lib/Scalar/List/Utils.pm
+++ lib/Scalar/List/Utils.pm
@@ -2,7 +2,7 @@ package Scalar::List::Utils;
 use strict;
 use warnings;
 
-our $VERSION    = "1.63";
+our $VERSION    = "1.67";
 $VERSION =~ tr/_//d;
 
 1;
